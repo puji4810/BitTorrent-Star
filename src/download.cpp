4e8df8be4ae2986a13988a295d58ff5a529ac2a9
puji <2430438586@qq.com>
2025-01-17 00:15:58 +0800

    修改了进度条100%后不会自动退出的bug

--- src/download.cpp
+++ src/download.cpp
@@ -105,7 +105,8 @@ inline int bitorrent_download(int argc, char *argv[]) {
 
 inline void check_torrent(lt::session &session, lt::torrent_status &st, indicators::ProgressBar &progress_bar)
 {
-  while (true)
+  bool torrent_finished = false; // 状态变量，标记是否完成
+  while (!torrent_finished)
   {
     session.post_torrent_updates();
     std::this_thread::sleep_for(std::chrono::seconds(1));
@@ -119,71 +120,63 @@ inline void check_torrent(lt::session &session, lt::torrent_status &st, indicato
       {
       case lt::add_torrent_alert::alert_type:
       {
-        // 使用 alert_cast 进行安全转换
         auto add_alert = lt::alert_cast<lt::add_torrent_alert>(alert);
         if (add_alert)
-        { // 确保转换成功
+        {
           std::cout << "Torrent added: " << add_alert->torrent_name() << "\n";
         }
-        break; // 确保控制流不会进入下一个 case
+        break;
       }
+
+      // case lt::torrent_finished_alert::alert_type:
+      // {
+      //   auto finished_alert = lt::alert_cast<lt::torrent_finished_alert>(alert);
+      //   if (finished_alert)
+      //   {
+      //     std::cout << "Torrent finished\n";
+      //     torrent_finished = true; // 标记完成
+      //     break;
+      //   }
+      // }
 
       case lt::state_update_alert::alert_type:
       {
         auto update_alert = lt::alert_cast<lt::state_update_alert>(alert);
-        if (update_alert)
-        { // 确保转换成功
-          if (update_alert && !update_alert->status.empty())
+        if (update_alert && !update_alert->status.empty())
+        {
+          st = update_alert->status.at(0);
+          progress_bar.set_progress(st.progress * 100);
+          double download_rate = st.download_rate / 1024.0;
+          std::string speed_str = (download_rate > 1024.0)
+                                      ? std::to_string(download_rate / 1024.0) + "MB/s"
+                                      : std::to_string(download_rate) + "KB/s";
+          progress_bar.set_option(indicators::option::PostfixText{
+              "Downloading... " + std::to_string(int(st.progress * 100)) +
+              "% | Speed: " + speed_str});
+          if(st.is_finished)
           {
-            // 只处理第一个状态更新
-            st = update_alert->status.at(0);
-            progress_bar.set_progress(st.progress * 100);
-            double download_rate = st.download_rate / 1024.0;
-            std::string speed_str;
-            if (download_rate > 1024.0)
-            {
-              speed_str = std::to_string(download_rate / 1024.0) + "MB/s";
-            }
-            else
-            {
-              speed_str = std::to_string(download_rate) + "KB/s";
-            }
-            progress_bar.set_option(indicators::option::PostfixText{
-                "Downloading... " + std::to_string(int(st.progress * 100)) +
-                "% | Speed: " + speed_str});
+            progress_bar.set_option(indicators::option::ForegroundColor{indicators::Color::cyan});
+            progress_bar.set_option(indicators::option::PostfixText{"Download complete!"});
+            progress_bar.mark_as_completed();
+            std::cout << "\nDownload complete!\n";
+            torrent_finished = true;
           }
-          std::cout << '\r' << std::flush;
-
-          break;
         }
+        break;
       }
 
       case lt::torrent_checked_alert::alert_type:
       {
         auto checked_alert = lt::alert_cast<lt::torrent_checked_alert>(alert);
         if (checked_alert)
-        { // 确保转换成功
+        {
           std::cout << "Torrent checked\n";
         }
         break;
       }
 
-      case lt::torrent_finished_alert::alert_type:
-      {
-        auto finished_alert = lt::alert_cast<lt::torrent_finished_alert>(alert);
-        if (finished_alert)
-        { // 确保转换成功
-          std::cout << "Torrent finished\n";
-          return; // 如果完成下载，退出程序
-        }
-        break;
-      }
-
       default:
-      {
-        // 对于未处理的 alert，确保安全结束
         break;
-      }
       }
     }
   }
@@ -199,7 +192,7 @@ inline int async_bitorrent_download(int argv, char *argc[]) {
   lt::add_torrent_params params;
   lt::torrent_status st;
   // session.apply_settings(settings);
-  set_session(session);
+  // set_session(session);
   auto ti = std::make_shared<lt::torrent_info>(argc[1]);
 
   indicators::ProgressBar progress_bar{
@@ -224,4 +217,5 @@ inline int async_bitorrent_download(int argv, char *argc[]) {
   std::cout << "Downloading to: " << params.save_path << "\n";
 
   check_torrent(session,st, progress_bar);
+  return 0;
 }
