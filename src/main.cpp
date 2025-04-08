c73e2db38091f9a1170dd858608a0188fa0c6b41
Puji <2430438586@qq.com>
2025-04-08 21:48:03 +0800

    add jsoncpp lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include "downloader.h"
 #include "initer.h"
+#include "json/json.h"
+#include <fstream>
+
 int main(int argc, char *argv[]) {
   initer initer(argc, argv);
   torrent_downloader td(initer.file_paths, initer.save_path);
