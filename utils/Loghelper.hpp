b5b4ab4e1f3f38ff66fb45b7f8ac1456843f0219
Puji <2430438586@qq.com>
2025-04-21 22:11:57 +0800

    log输出重定向到log文件中

--- utils/Loghelper.hpp
+++ utils/Loghelper.hpp
@@ -5,11 +5,28 @@
 #ifndef LOGHELPER_H
 #define LOGHELPER_H
 
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <iostream>
 
 
-class Loghelper {
 
-};
+     void Loginit(){
+        try {
+            // 创建一个文件日志记录器
+            auto file_logger = spdlog::basic_logger_mt("file_logger", "logs/logfile.log");
+
+            // 设置日志格式（可选）
+            file_logger->set_pattern("[%Y-%m-%d %H:%M:%S] [%l] %v");
+
+            // 设置为默认日志记录器
+            spdlog::set_default_logger(file_logger);
+
+        } catch (const spdlog::spdlog_ex &ex) {
+            std::cerr << "Log initialization failed: " << ex.what() << std::endl;
+        }
+      }
+
 
 
 
