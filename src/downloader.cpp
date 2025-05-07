0f6c3fc688247cc2f096b3e412d60048a5ae8d56
Puji <2430438586@qq.com>
2025-05-07 17:44:37 +0800

    fix:change tasks and bars to static, so that the tasks more than 10 can start its task

--- src/downloader.cpp
+++ src/downloader.cpp
@@ -6,7 +6,8 @@ std::size_t torrent_downloader::bar_index = 0;
 
 indicators::DynamicProgress<indicators::ProgressBar> torrent_downloader::bars{};
 
-void torrent_downloader::set_session(lt::session &session) {
+void torrent_downloader::set_session(lt::session &session)
+{
     lt::settings_pack settings;
     // 调整发送/接收缓冲区
     settings.set_int(lt::settings_pack::send_buffer_watermark, 1 * 1024 * 1024);
@@ -18,7 +19,7 @@ void torrent_downloader::set_session(lt::session &session) {
     settings.set_int(lt::settings_pack::active_downloads, 10);
     settings.set_int(lt::settings_pack::active_seeds, 10);
 
-    settings.set_bool(lt::settings_pack::enable_dht, true); // 确保DHT功能启用
+    settings.set_bool(lt::settings_pack::enable_dht, true);        // 确保DHT功能启用
     settings.set_int(lt::settings_pack::dht_upload_rate_limit, 0); // 不限制DHT上传速率
 
     settings.set_int(lt::settings_pack::alert_mask, lt::alert::all_categories);
@@ -26,7 +27,8 @@ void torrent_downloader::set_session(lt::session &session) {
     session.apply_settings(settings);
 }
 
-void torrent_downloader::check_torrent_status(Task &tk) {
+void torrent_downloader::check_torrent_status(Task &tk)
+{
     lt::session &session = *tk.session;
     lt::torrent_status &st = tk.status;
     std::size_t bars_index = tk.bar_index;
@@ -35,108 +37,126 @@ void torrent_downloader::check_torrent_status(Task &tk) {
     session.post_torrent_updates();
     std::this_thread::sleep_for(std::chrono::milliseconds(50));
     session.pop_alerts(&alerts);
-    for (auto alert: alerts) {
-        switch (alert->type()) {
-            // 处理 torrent 错误
-            case lt::torrent_error_alert::alert_type: {
-                auto error_alert = lt::alert_cast<lt::torrent_error_alert>(alert);
-                if (error_alert) {
-                    spdlog::error("Torrent error: {}", error_alert->error.message());
-                }
-                break;
+    for (auto alert : alerts)
+    {
+        switch (alert->type())
+        {
+        // 处理 torrent 错误
+        case lt::torrent_error_alert::alert_type:
+        {
+            auto error_alert = lt::alert_cast<lt::torrent_error_alert>(alert);
+            if (error_alert)
+            {
+                spdlog::error("Torrent error: {}", error_alert->error.message());
             }
+            break;
+        }
 
-            // 处理元数据解析失败
-            case lt::metadata_failed_alert::alert_type: {
-                auto metadata_alert = lt::alert_cast<lt::metadata_failed_alert>(alert);
-                if (metadata_alert) {
-                    spdlog::error("Failed to retrieve metadata for torrent: {}. Error: {}",
-                                  metadata_alert->handle.info_hashes().get_best().to_string(),
-                                  metadata_alert->error.message());
-                }
-                break;
+        // 处理元数据解析失败
+        case lt::metadata_failed_alert::alert_type:
+        {
+            auto metadata_alert = lt::alert_cast<lt::metadata_failed_alert>(alert);
+            if (metadata_alert)
+            {
+                spdlog::error("Failed to retrieve metadata for torrent: {}. Error: {}",
+                              metadata_alert->handle.info_hashes().get_best().to_string(),
+                              metadata_alert->error.message());
             }
+            break;
+        }
 
-            // 处理 tracker 错误
-            case lt::tracker_error_alert::alert_type: {
-                auto tracker_alert = lt::alert_cast<lt::tracker_error_alert>(alert);
-                if (tracker_alert) {
-                    spdlog::error("Tracker error: {}", tracker_alert->error.message());
-                }
-                break;
+        // 处理 tracker 错误
+        case lt::tracker_error_alert::alert_type:
+        {
+            auto tracker_alert = lt::alert_cast<lt::tracker_error_alert>(alert);
+            if (tracker_alert)
+            {
+                spdlog::error("Tracker error: {}", tracker_alert->error.message());
             }
+            break;
+        }
 
-            // 处理文件错误
-            case lt::file_error_alert::alert_type: {
-                auto file_alert = lt::alert_cast<lt::file_error_alert>(alert);
-                if (file_alert) {
-                    spdlog::error("File error: {}",
-                                  file_alert->error.message());
-                }
-                break;
+        // 处理文件错误
+        case lt::file_error_alert::alert_type:
+        {
+            auto file_alert = lt::alert_cast<lt::file_error_alert>(alert);
+            if (file_alert)
+            {
+                spdlog::error("File error: {}",
+                              file_alert->error.message());
             }
+            break;
+        }
 
-            // 其他已有的警报处理逻辑
-            case lt::torrent_checked_alert::alert_type: {
-                auto checked_alert = lt::alert_cast<lt::torrent_checked_alert>(alert);
-                if (checked_alert) {
-                    spdlog::info("Torrent checked: {}",
-                                 checked_alert->handle.info_hashes().get_best().to_string());
-                }
-                break;
+        // 其他已有的警报处理逻辑
+        case lt::torrent_checked_alert::alert_type:
+        {
+            auto checked_alert = lt::alert_cast<lt::torrent_checked_alert>(alert);
+            if (checked_alert)
+            {
+                spdlog::info("Torrent checked: {}",
+                             checked_alert->handle.info_hashes().get_best().to_string());
             }
+            break;
+        }
 
-            case lt::state_update_alert::alert_type: {
-                auto update_alert = lt::alert_cast<lt::state_update_alert>(alert);
-                if (update_alert && !update_alert->status.empty()) {
-                    st = update_alert->status.at(0);
-                    if ( tk.params.name != st.name) {
-                        break;
-                    }
-                    bars[bars_index].set_progress(st.progress * 100);
-                    double download_rate = st.download_rate / 1024.0;
-                    std::string speed_str =
-                            (download_rate > 1024.0)
-                                ? std::to_string(download_rate / 1024.0) + "MB/s"
-                                : std::to_string(download_rate) + "KB/s";
-                    bars[bars_index].set_option(indicators::option::PostfixText{
-                        "Downloading... " + std::to_string(static_cast<int>(st.progress * 100)) +
-                        "% | Speed: " + speed_str + " | [" + st.name.substr(0, 40) + "]"
-                    });
-                    if (st.is_finished) {
-                        bars[bars_index].set_option(
-                            indicators::option::ForegroundColor{indicators::Color::cyan});
-                        bars[bars_index].set_option(
-                            indicators::option::PostfixText{"Download complete!"});
-                        bars[bars_index].mark_as_completed();
-                        tk.is_finished = true;
-                        return;
-                    }
+        case lt::state_update_alert::alert_type:
+        {
+            auto update_alert = lt::alert_cast<lt::state_update_alert>(alert);
+            if (update_alert && !update_alert->status.empty())
+            {
+                st = update_alert->status.at(0);
+                if (tk.params.name != st.name)
+                {
+                    break;
                 }
-                break;
+                bars[bars_index].set_progress(st.progress * 100);
+                double download_rate = st.download_rate / 1024.0;
+                std::string speed_str =
+                    (download_rate > 1024.0)
+                        ? std::to_string(download_rate / 1024.0) + "MB/s"
+                        : std::to_string(download_rate) + "KB/s";
+                bars[bars_index].set_option(indicators::option::PostfixText{
+                    "Downloading... " + std::to_string(static_cast<int>(st.progress * 100)) +
+                    "% | Speed: " + speed_str + " | [" + st.name.substr(0, 40) + "]"});
+                if (st.is_finished)
+                {
+                    bars[bars_index].set_option(
+                        indicators::option::ForegroundColor{indicators::Color::cyan});
+                    bars[bars_index].set_option(
+                        indicators::option::PostfixText{"Download complete!"});
+                    bars[bars_index].mark_as_completed();
+                    tk.is_finished = true;
+                    return;
+                }
             }
+            break;
+        }
 
-            default:
-                break;
+        default:
+            break;
         }
     }
 }
 
-void torrent_downloader::check_torrent_polling() {
-    while (!tasks.empty()) {
-        for (size_t i = 0; i < tasks.size(); ++i) {
+void torrent_downloader::check_torrent_polling()
+{
+    while (!tasks.empty())
+    {
+        for (size_t i = 0; i < tasks.size(); ++i)
+        {
             check_torrent_status(tasks[i]);
         }
 
-        std::erase_if(tasks, [](const auto &task) {
-            return task.is_finished;
-        });
+        std::erase_if(tasks, [](const auto &task)
+                      { return task.is_finished; });
     }
 }
 
-
-void torrent_downloader::async_bitorrent_download() {
-    for (auto &task: tasks) {
+void torrent_downloader::async_bitorrent_download()
+{
+    for (auto &task : tasks)
+    {
         // set_session(task.session);
         task.session->async_add_torrent(task.params);
         task.session->post_torrent_updates();
--- src/downloader.h
+++ src/downloader.h
@@ -15,10 +15,12 @@
 #include <thread>
 #include <vector>
 #include <fstream>
+#include <mutex>
 #include <fmt/format.h>
 #include "spdlog/spdlog.h"
 
-struct Task {
+struct Task
+{
     std::unique_ptr<libtorrent::session> session;
     libtorrent::add_torrent_params params;
     libtorrent::torrent_status status;
@@ -26,7 +28,8 @@ struct Task {
     std::size_t bar_index;
     bool is_finished = false;
 
-    Task() {
+    Task()
+    {
         session = std::make_unique<libtorrent::session>();
     }
 
@@ -34,17 +37,20 @@ struct Task {
 
     Task &operator=(const Task &) = delete;
 
-    Task(Task&& other) noexcept
+    Task(Task &&other) noexcept
         : session(std::move(other.session)),
           params(std::move(other.params)),
           status(std::move(other.status)),
           alerts(std::move(other.alerts)),
           bar_index(other.bar_index),
           is_finished(other.is_finished)
-    {}
+    {
+    }
 
-    Task& operator=(Task&& other) noexcept {
-        if (this != &other) {
+    Task &operator=(Task &&other) noexcept
+    {
+        if (this != &other)
+        {
             session = std::move(other.session);
             params = std::move(other.params);
             status = std::move(other.status);
@@ -58,73 +64,134 @@ struct Task {
     ~Task() = default;
 };
 
-// struct TaskManager {
-//
-// };
+namespace puji
+{
+    struct Bars
+    {
+    public:
+        Bars() = default;
 
-struct torrent_downloader {
+        std::size_t add_bars(std::unique_ptr<indicators::ProgressBar> bar)
+        {
+            bars.emplace_back(std::move(bar));
+            return this->size();
+        }
+
+        std::size_t size()
+        {
+            return bars.size();
+        }
+
+        indicators::ProgressBar &get_bar(std::size_t bar_index)
+        {
+            return *bars.at(bar_index);
+        }
+
+    private:
+        std::vector<std::unique_ptr<indicators::ProgressBar>> bars;
+    };
+
+    static inline Bars &getBars()
+    {
+        static Bars bars;
+        return bars;
+    }
+
+    struct TaskManager
+    {
+        // 需要线程安全的manager
+    public:
+        void add_task(const std::string &src, const std::string &save_path)
+        {
+            std::lock_guard<std::mutex> lock(mutex); // 加锁
+
+            libtorrent::add_torrent_params params;
+            Task tk;
+
+            if (is_magnet_uri(src))
+            {
+                params = libtorrent::parse_magnet_uri(src);
+            }
+            else
+            {
+                params.ti = std::make_shared<libtorrent::torrent_info>(src);
+            }
+            params.trackers = trackers;
+            params.save_path = save_path;
+            tk.params = params;
+
+            // 添加进度条并获取索引
+            tk.bar_index = getBars().add_bars(std::make_unique<indicators::ProgressBar>(
+                indicators::option::BarWidth{50},
+                indicators::option::Start{"["},
+                indicators::option::Fill{"="},
+                indicators::option::Lead{">"},
+                indicators::option::Remainder{" "},
+                indicators::option::End{"]"},
+                indicators::option::ForegroundColor{indicators::Color::cyan},
+                indicators::option::ShowElapsedTime{true},
+                indicators::option::ShowRemainingTime{true},
+                indicators::option::FontStyles{
+                    std::vector<indicators::FontStyle>{indicators::FontStyle::bold}}));
+
+            tasks.emplace_back(std::move(tk));
+        }
+
+        void remove_task()
+        {
+            std::lock_guard<std::mutex> lock(mutex); // 加锁
+            std::erase_if(tasks, [](const auto &task)
+                          { return task.is_finished; });
+        }
+
+        bool empty() const
+        {
+            return tasks.empty();
+        }
+
+    private:
+        std::vector<Task> tasks;
+        indicators::DynamicProgress<indicators::ProgressBar> bars;
+        // std::size_t bar_index = 0;
+        std::vector<std::string> trackers;
+        std::mutex mutex;
+
+        bool is_magnet_uri(const std::string &str)
+        {
+            return str.find("magnet:?xt=urn:btih:") == 0;
+        }
+    };
+
+    static inline TaskManager &getTaskManager()
+    {
+        static TaskManager manager;
+        return manager;
+    }
+}
+
+struct torrent_downloader
+{
 public:
     static std::vector<std::string> trackers;
     static std::size_t bar_index;
 
-    torrent_downloader(const std::string &src,
-                       const std::string &save_path = "./download") {
-        libtorrent::add_torrent_params params;
-        Task tk;
-
-        if (is_magnet_uri(src)) {
-            params = libtorrent::parse_magnet_uri(src);
-        } else {
-            params.ti = std::make_shared<libtorrent::torrent_info>(src);
-        }
-        params.trackers = trackers;
-        params.save_path = save_path;
-        tk.params = params;
-        tk.bar_index = bar_index++;
-        bars.push_back(std::make_unique<indicators::ProgressBar>(
-                indicators::option::BarWidth{50}, indicators::option::Start{"["},
-                indicators::option::Fill{"="}, indicators::option::Lead{">"},
-                indicators::option::Remainder{" "}, indicators::option::End{"]"},
-                indicators::option::ForegroundColor{indicators::Color::cyan},
-                indicators::option::ShowElapsedTime{true},
-                indicators::option::ShowRemainingTime{true},
-                indicators::option::FontStyles{
-                    std::vector<indicators::FontStyle>{indicators::FontStyle::bold}
-                }
-            ));
-        tasks.emplace_back(std::move(tk));
+    explicit torrent_downloader(const std::string &src,
+                                const std::string &save_path = "./download")
+    {
+        add_task(src, save_path);
     }
 
-    torrent_downloader(const std::vector<std::string> &src,
-                       const std::string &save_path = "./download") {
-        for (const auto &s: src) {
-            libtorrent::add_torrent_params params;
-            Task tk;
-            if (is_magnet_uri(s)) {
-                params = libtorrent::parse_magnet_uri(s);
-            } else {
-                params.ti = std::make_shared<libtorrent::torrent_info>(s);
-            }
-            params.trackers = trackers;
-            params.save_path = save_path;
-            tk.params = std::move(params);
-            tk.bar_index = bar_index++;
-            bars.push_back(std::make_unique<indicators::ProgressBar>(
-                indicators::option::BarWidth{50}, indicators::option::Start{"["},
-                indicators::option::Fill{"="}, indicators::option::Lead{">"},
-                indicators::option::Remainder{" "}, indicators::option::End{"]"},
-                indicators::option::ForegroundColor{indicators::Color::cyan},
-                indicators::option::ShowElapsedTime{true},
-                indicators::option::ShowRemainingTime{true},
-                indicators::option::FontStyles{
-                    std::vector<indicators::FontStyle>{indicators::FontStyle::bold}
-                }
-            ));
-            tasks.emplace_back(std::move(tk));
+    explicit torrent_downloader(const std::vector<std::string> &src,
+                                const std::string &save_path = "./download")
+    {
+        for (const auto &s : src)
+        {
+            add_task(s, save_path);
         }
     }
 
-    void wait() {
+    void wait()
+    {
         check_torrent_polling();
     }
 
@@ -142,8 +209,38 @@ private:
 
     void check_torrent_polling();
 
-    bool is_magnet_uri(const std::string &str) {
+    bool is_magnet_uri(const std::string &str)
+    {
         return str.find("magnet:?xt=urn:btih:") == 0; // 判断是否以 "magnet:?xt=urn:btih:" 开头
+    }
+
+    void add_task(const std::string &src, const std::string &save_path)
+    {
+        libtorrent::add_torrent_params params;
+        Task tk;
+
+        if (is_magnet_uri(src))
+        {
+            params = libtorrent::parse_magnet_uri(src);
+        }
+        else
+        {
+            params.ti = std::make_shared<libtorrent::torrent_info>(src);
+        }
+        params.trackers = trackers;
+        params.save_path = save_path;
+        tk.params = params;
+        tk.bar_index = bar_index++;
+        bars.push_back(std::make_unique<indicators::ProgressBar>(
+            indicators::option::BarWidth{50}, indicators::option::Start{"["},
+            indicators::option::Fill{"="}, indicators::option::Lead{">"},
+            indicators::option::Remainder{" "}, indicators::option::End{"]"},
+            indicators::option::ForegroundColor{indicators::Color::cyan},
+            indicators::option::ShowElapsedTime{true},
+            indicators::option::ShowRemainingTime{true},
+            indicators::option::FontStyles{
+                std::vector<indicators::FontStyle>{indicators::FontStyle::bold}}));
+        tasks.emplace_back(std::move(tk));
     }
 };
 
